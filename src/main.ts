c14b50c9c90206969207f4c5d6cdb4c84ee4a9ee
lea-apotheloz <apothelozlea2@gmail.com>
2025-01-07 15:39:54 +0100

    style: format code

--- src/main.ts
+++ src/main.ts
@@ -2,30 +2,29 @@ import './style.css'
 
 console.log('Hello from typescript')
 document.addEventListener('DOMContentLoaded', () => {
-  const todoInput = document.querySelector<HTMLInputElement>('#todo-input');
-  const addTodoButton = document.querySelector('#add-todo-button');
-  const todoList = document.querySelector('#todo-list');
+  const todoInput = document.querySelector<HTMLInputElement>('#todo-input')
+  const addTodoButton = document.querySelector('#add-todo-button')
+  const todoList = document.querySelector('#todo-list')
 
   function addTodo() {
     if (todoInput && todoList) {
-    const todoText = todoInput.value.trim();
-    if (todoText) {
-      const li = document.createElement('li');
-      li.textContent = todoText;
-      li.className = 'todo-element';
-      todoList.appendChild(li);
-      todoInput.value = '';
-    }
+      const todoText = todoInput.value.trim()
+      if (todoText) {
+        const li = document.createElement('li')
+        li.textContent = todoText
+        li.className = 'todo-element'
+        todoList.appendChild(li)
+        todoInput.value = ''
+      }
     }
   }
-  if (addTodoButton  && todoInput) {
-    addTodoButton.addEventListener('click', addTodo);
+  if (addTodoButton && todoInput) {
+    addTodoButton.addEventListener('click', addTodo)
 
     todoInput.addEventListener('keypress', (e) => {
       if (e.key === 'Enter') {
-        addTodo();
+        addTodo()
       }
-    });
+    })
   }
-});
-
+})
