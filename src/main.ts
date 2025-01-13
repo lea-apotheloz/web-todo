a62d0c75e4eddd73cad4ddcca10417b41016f5a1
lea-apotheloz <apothelozlea2@gmail.com>
2025-01-13 16:55:45 +0100

    feat: add remove individual todos
    closes #2

--- src/main.ts
+++ src/main.ts
@@ -3,25 +3,43 @@ interface Todo {
   text: string
 }
 console.log('Hello from typescript')
-document.addEventListener('DOMContentLoaded', () => {
+//document.addEventListener('DOMContentLoaded', () => {
   const todoInput = document.querySelector<HTMLInputElement>('#todo-input')
   const addTodoButton = document.querySelector('#add-todo-button')
   const todoList = document.querySelector('#todo-list')
   const deserialized = localStorage.getItem('value')
 
+
+
   let todos: Todo[] = []
   if (deserialized) todos = JSON.parse(deserialized)
   todos.forEach(addTodo)
 
-  function addTodo(todo: Todo) {
+  function addTodo(todo: Todo, index: number) {
     if (todoList) {
       const todoText = todo.text
       if (todoText) {
         const li = document.createElement('li')
         li.textContent = todoText
+
+        const deleteButton = document.createElement('button')
+        deleteButton.textContent = 'delete'
+        deleteButton.addEventListener('click', () => {
+          deleteTodo(index)
+        })
+        li.appendChild(deleteButton)
+
         li.className = 'todo-element'
         todoList.appendChild(li)
       }
+    }
+  }
+  function deleteTodo(index: number){
+    if (todoList) {
+      todos.splice(index, 1)
+      localStorage.setItem('value', JSON.stringify(todos))
+      todoList.remove()
+      todos.forEach(addTodo)
     }
   }
   if (addTodoButton && todoInput) {
@@ -41,9 +59,10 @@ document.addEventListener('DOMContentLoaded', () => {
         todos.push(newtodo)
         const serialized = JSON.stringify(todos)
         localStorage.setItem('value', serialized)
-        addTodo(newtodo)
         todoInput.value = ''
+        addTodo(newtodo, todos.length - 1)
+
       }
     }
   }
-})
+//})
